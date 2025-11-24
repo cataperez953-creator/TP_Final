e3f592ab1e874d75cb9b1bd4b0283356f40748d0
cataperez953-creator <cataperez953@gmail.com>
2025-11-24 16:55:54 -0300

    Add incremental logic to bubble sort algorithm
    Implemented incremental bubble sort logic in the step function, allowing for partial sorting with each call.

--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -13,11 +13,27 @@ def init(vals):
     j = 0
 
 def step():
-    # TODO:
-    # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
+   global items, n, i, j  # TODO: implementamos bubble sort incremental, donde cada llamada a step() hace solo un par de indices.
+# si ya se completaron todas las pasadas del bubble sort
+    if i>=n-1:
+        return {"done": True}
+# 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
+    a=j
+    b=j+1
+    swap= False 
     # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
-    # 3) Avanzar punteros (preparar el próximo paso).
-    # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
-    #
-    # Cuando no queden pasos, devolvé {"done": True}.
-    return {"done": True}
+    if items[a]>items[b]:
+        items[a], items[b]= items[b], items[a]
+# 3) Avanzar punteros (preparar el próximo paso).
+    j=j+1
+ #termino la pasada i --> reiniciar j y avanzar i
+    if j>=n-i-1:
+        j=0
+        i=i+1
+# 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
+    return {
+        "a": a, 
+        "b": b, 
+        "swap": swap, 
+        "done": False
+    }
