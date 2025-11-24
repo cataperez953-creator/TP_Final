821a46dca755cd4fd5677594dde9c7d616e307d9
cataperez953-creator <cataperez953@gmail.com>
2025-11-24 17:41:16 -0300

    Refactor step function in bubble sort algorithm
    Refactor bubble sort step function for incremental processing.

--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -13,27 +13,8 @@ def init(vals):
     j = 0
 
 def step():
-   global items, n, i, j  # TODO: implementamos bubble sort incremental, donde cada llamada a step() hace solo un par de indices.
-# si ya se completaron todas las pasadas del bubble sort
-    if i>=n-1:
-        return {"done": True}
+    # TODO: implementamos bubble sort incremental, donde cada llamada a step() hace solo un par de indices.
 # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
-    a=j
-    b=j+1
-    swap= False 
-    # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
-    if items[a]>items[b]:
-        items[a], items[b]= items[b], items[a]
-# 3) Avanzar punteros (preparar el próximo paso).
-    j=j+1
- #termino la pasada i --> reiniciar j y avanzar i
-    if j>=n-i-1:
-        j=0
-        i=i+1
+# 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
+# 3) Avanzar punteros (preparar el próximo pa
 # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
-    return {
-        "a": a, 
-        "b": b, 
-        "swap": swap, 
-        "done": False
-    }
